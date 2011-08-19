6ba0ecb8a8915c85eabdbcaeef2d0acd926ff4c5
kbkbkbkbkb1 <kbkbkbkb1@gmail.com>
2011-08-19 19:12:30 +0900

    とりあえずタイピング機能はひと通り実装した．修正しすぎてファイル全体で1hunkになってしまった．
    * 実装した機能
    ** ほぼすべてのローマ字入力に対応
    拗音，促音，撥音などの特殊拍も含めてだいたい標準的なローマ字入力法には対応できているはず．
    とりあえず思いつく対応しきれていない点は，「っち」を tti とか cchi で入力できるが，
    tchi や cti でも入力できてしまう点が問題．
    ** すでに入力したローマ字やひらがなの色を変更
    すでに入力したものや，今入力すべきローマ字，ひらがなの色変更ができる．まともに自分で
    face を書いたのは初めてだ．
    ** タイピング時間を測るストップウォッチ
    リアルタイムで時間が進むストップウォッチでないと雰囲気がでないので，concurrent.el を
    使って非同期なストップウォッチを実装した．
    
    * TODO
    ** 練習の方式をどうするか決める
    今はランダムに20個問題を並べているだけだが，タイプウェルのようにタイプ数で1練習に
    したい．
    ** 問題文を集める
    めんどくさい
    ** 入力すべきローマ字全体を表示
    現在入力すべきローマ字候補は表示しているが，問題全体で入力すべきローマ字を並べないと
    意味が無い．ついでに，入力したものの色を変更できるようにしないとね．
    ** 漢字の問題文についても色変更
    ひらがな問題文はタイプに準じて色変更できるが，漢字だと境目がわからないので色変更
    できない．どうやったらいいもんか．
    ** 測った時間をどう集計するか
    時間は測れても，それをどうやって保存して，どうやってユーザに提示するかが悩みどころ．
    普通にテキストに書きだせばいいかね．あと，1タイプごとに時間を取るべきですよね．うーん．
    グラフも作れたらかっこいーな．

--- typeJR.el
+++ typeJR.el
@@ -1,163 +1,392 @@
 
+(defvar typeJR-mondai-alist nil)
+(defvar typeJR-kanji-list nil)
+(defvar typeJR-random-times 20)
+(defvar typeJR-cur-mondai nil)
+(defvar typeJR-cur-inputed-mondai "")
+(defvar typeJR-cur-hiragana "")
+(defvar typeJR-cur-hiragana-pos 0)
+(defvar typeJR-cur-romaji-candidates nil)
+(defvar typeJR-cur-romaji-pos 0)
+(defvar typeJR-cur-char nil)
+(defvar typeJR-romaji-definition nil)
+(defvar typeJR-sokuon "っ")
+(defvar typeJR-hatuon "ん")
+(defvar typeJR-na-gyou '("な" "に" "ぬ" "ね" "の" "にゃ" "にぃ" "にゅ" "にぇ" "にょ" "ん"))
+(defvar typeJR-boin '("あ" "い" "う" "え" "お"))
+(defvar typeJR-nn-flag nil)
+(defvar typeJR-wrong-flag nil)
+(defvar typeJR-mode-map nil "local map for typeJR mode")
 
-(defvar typejr-mondai-alist
-  '(("懇親会" . "こんしんかい")
-    ("日付" . "ひづけ")
-    ("東京特許許可局" . "とうきょうとっきょきょかきょく")
-    ))
+(defface typeJR-good-romaji-face
+  '((t (:foreground "gray60"))) nil)
 
-(defvar typejr-cur-mondai "きゃんぷふぁいあー")
-(defvar typejr-cur-inputed-mondai "")
-(defvar typejr-cur-hiragana "")
-(defvar typejr-cur-hiragana-pos 0)
-(defvar typejr-cur-romaji-candidates nil)
-(defvar typejr-cur-romaji-pos 0)
-(defvar typejr-cur-char nil)
-(defvar typejr-romaji-definition nil)
+(defface typeJR-wrong-romaji-face
+  '((t (:foreground "cyan" :background "red"))) nil)
 
-(defface typejr-good-romaji-face
-  '((t (:foreground "red"))) nil)
+(defface typeJR-cur-romaji-face
+  '((t (:foreground "cyan"))) nil)
 
-(defun typejr-initial ()
-  ""
-  (typejr-read-romaji-definition))
 
-(defun typejr-mondai-loop ()
+(defun typeJR-practice-prepare ()
   ""
   (interactive)
-  (setq typejr-cur-hiragana-pos 0)
-  (setq typejr-cur-inputed-mondai "")
-  (typejr-rewrite-mondai)
-  (typejr-rewrite-evaluation "")
-  (while (not (typejr-end-of-mondai-p))
-    (setq typejr-cur-hiragana
-          (substring typejr-cur-mondai typejr-cur-hiragana-pos
-                     (1+ typejr-cur-hiragana-pos)))
-    (setq typejr-cur-romaji-candidates
-          (typejr-get-romaji-candidates typejr-cur-hiragana))
-    (typejr-rewrite-inputed-mondai)
-    (typejr-rewrite-hiragana)
-    (typejr-hiragana-loop)
-    (setq typejr-cur-hiragana-pos (1+ typejr-cur-hiragana-pos))
-    (setq typejr-cur-inputed-mondai
-          (concat typejr-cur-inputed-mondai typejr-cur-hiragana))))
+  (erase-buffer)
+  (goto-char (point-min))
+  (insert "
 
-(defun typejr-hiragana-loop ()
-  ""
-  (setq typejr-cur-romaji-pos 0)
-  (while (not (typejr-end-of-hiragana-p))
-    (typejr-rewrite-romaji-candidates)
-    (typejr-rewrite-romaji-pos)
-    (let* ((inputed-romaji (read-char))
-           (correct-list (typejr-get-correct-candidate-list inputed-romaji)))
-      (if (> (length correct-list) 0)
-          (progn
-            (setq typejr-cur-romaji-pos
-                  (1+ typejr-cur-romaji-pos))
-            (setq typejr-cur-romaji-candidates correct-list)
-            (insert inputed-romaji)
-            (typejr-rewrite-evaluation "Good!"))
-        (let ((visible-bell nil)
-              (ring-bell-function 'beep))
-          (ding)
-          (typejr-rewrite-evaluation "Bad"))))))
+Evaluation: 
+Type: 
 
-(defun typejr-end-of-mondai-p ()
-  ""
-  (<= (length typejr-cur-mondai) typejr-cur-hiragana-pos))
+ひらがな問題文                ：
+いままで入力した問題文        ：
+次にうつべきひらがな          ：
+次にうつべきローマ字の候補    ：
+ローマ字何文字目まで入力したか："))
 
-(defun typejr-end-of-hiragana-p ()
+(defun typeJR-initial ()
   ""
-  (<= (car (sort (mapcar 'length typejr-cur-romaji-candidates) '<))
-      typejr-cur-romaji-pos))
+  (interactive)
+  ;; configuration buffer and major mode
+  (switch-to-buffer "*typeJR*")
+  (setq major-mode 'typeJR-mode)
+  (setq mode-name "typeJR")
+  (setq typeJR-mode-map (make-sparse-keymap))
+  (define-key typeJR-mode-map (kbd "C-c C-p") 'typeJR-practice)
+  (use-local-map typeJR-mode-map)
+  (typeJR-read-romaji-definition)
+  (typeJR-read-mondai-alist)
+  (typeJR-practice-prepare)
+  (font-lock-mode 0))
 
-(defun typejr-get-cur-hiragana ()
+(defun typeJR-practice ()
   ""
-  (let* ((len (length typejr-cur-mondai))
+  (interactive)
+  (typeJR-clear-type)
+  (random t)                            ; change seed
+  (setq typeJR-kanji-list
+        (mapcar '(lambda (x)
+                   (car (nth (random x) typeJR-mondai-alist)))
+                (make-list typeJR-random-times (length typeJR-mondai-alist))))
+  (typeJR-rewrite-kanji)
+  (condition-case err
+      (progn
+        (typeJR-stop-watch-start)
+        (let ((cnt 0)
+              (echo-keystrokes 0))            ; don't echo key type in echo area
+          (while (< cnt typeJR-random-times)
+            (setq typeJR-cur-mondai
+                  (cdr (assoc (nth cnt typeJR-kanji-list) typeJR-mondai-alist)))
+            (typeJR-mondai-loop)
+            ;; input space between mondais
+            (unless (= cnt (1- typeJR-random-times))
+              (let (inputed-char)
+                (while (not (= (setq inputed-char (read-char)) ? ))
+                  (if (and typeJR-nn-flag (= inputed-char ?n))
+                      ;; accept "n"
+                      (progn
+                        (setq typeJR-nn-flag nil)
+                        (insert inputed-char))
+                    (setq typeJR-wrong-flag t)
+                    (typeJR-beep-when-wrong)
+                    (typeJR-rewrite-romaji-candidates)))
+                (insert inputed-char)))                       ; insert space
+            (setq cnt (1+ cnt)))
+          (cc:signal-send typeJR-stop-watch-channel 'stop)
+          (typeJR-rewrite-evaluation "Finish!")))
+    (quit
+     (cc:signal-send typeJR-stop-watch-channel 'stop)
+     (error "Typing is interrupted"))))
+
+(defun typeJR-mondai-loop ()
+  ""
+  (interactive)
+  ;; pre processing
+  (setq typeJR-cur-hiragana-pos 0)
+  (setq typeJR-cur-inputed-mondai "")
+  (setq typeJR-nn-flag nil)
+  (setq typeJR-wrong-flag nil)
+  (typeJR-rewrite-inputed-mondai)
+  (typeJR-rewrite-evaluation "")
+  (while (not (typeJR-end-of-mondai-p))
+    (setq typeJR-cur-hiragana
+          (typeJR-get-cur-hiragana))
+    (setq typeJR-cur-romaji-candidates
+          (typeJR-get-romaji-candidates typeJR-cur-hiragana))
+    (typeJR-rewrite-mondai)
+    ;; deal with special mora
+    (cond ((string= typeJR-cur-hiragana typeJR-sokuon)
+           (typeJR-deal-with-sokuon))
+          ((string= typeJR-cur-hiragana typeJR-hatuon)
+           (typeJR-deal-with-hatuon)))
+    (typeJR-rewrite-hiragana)
+    (typeJR-hiragana-loop)
+    ;; deal with nn-flag
+    (typeJR-deal-with-nn-flag)
+    (setq typeJR-cur-hiragana-pos
+          (+ typeJR-cur-hiragana-pos (length typeJR-cur-hiragana)))
+    (setq typeJR-cur-inputed-mondai
+          (concat typeJR-cur-inputed-mondai typeJR-cur-hiragana))
+    (typeJR-rewrite-inputed-mondai))
+  ;; post processing
+  (typeJR-rewrite-mondai)
+  (typeJR-rewrite-inputed-mondai)
+  (typeJR-rewrite-evaluation "Mondai ends!"))
+
+(defun typeJR-hiragana-loop ()
+  ""
+  (setq typeJR-cur-romaji-pos 0)
+  (typeJR-rewrite-romaji-candidates)
+  (typeJR-rewrite-romaji-pos)
+  (while (not (typeJR-end-of-hiragana-p))
+    (let* ((inputed-romaji (read-char)) ; recieve keyboard input
+           (correct-list (typeJR-get-correct-candidate-list inputed-romaji)))
+      (cond ((or (and typeJR-nn-flag (= inputed-romaji ?n))   ; accept "n"
+                 (> (length correct-list) 0))           ; correct input
+             (when (> (length correct-list) 0)
+                 (setq typeJR-cur-romaji-pos
+                       (1+ typeJR-cur-romaji-pos))
+                 (setq typeJR-cur-romaji-candidates correct-list))
+             (setq typeJR-nn-flag nil)
+             (setq typeJR-wrong-flag nil)
+             (insert inputed-romaji)
+             (typeJR-rewrite-romaji-candidates)
+             (typeJR-rewrite-mondai)
+             (typeJR-rewrite-romaji-pos)
+             (typeJR-rewrite-evaluation "Good!"))
+            (t                          ; wrong input
+             (setq typeJR-wrong-flag t)
+             (typeJR-beep-when-wrong)
+             (typeJR-rewrite-romaji-candidates)
+             (typeJR-rewrite-mondai))))))
+
+(defun typeJR-beep-when-wrong ()
+  (let ((visible-bell nil)
+        (ring-bell-function 'beep))
+    (beep)
+    (typeJR-rewrite-evaluation "Wrong")))
+
+(defun typeJR-deal-with-sokuon ()
+  "Add initial romaji of next hiragana to `typeJR-cur-romaji-candidates' to deal with sokuon."
+  (let* ((next-hiragana (typeJR-get-next-hiragana))
+         (next-candidates (typeJR-get-romaji-candidates next-hiragana))
+         new-candidates)
+    (if (or (member next-hiragana typeJR-na-gyou)
+            (member next-hiragana typeJR-boin))
+        nil
+      ;; version to concat sokuon and next hiragana.
+      ;; (mapc '(lambda (pre)
+      ;;          (mapc '(lambda (post)
+      ;;                   (add-to-list 'new-candidates (concat pre post)))
+      ;;                next-candidates))
+      ;;       typeJR-cur-romaji-candidates)
+      ;; (nreverse new-candidates)
+      (setq new-candidates (mapcar '(lambda (candidate) (substring candidate 0 1))
+                                   next-candidates)) ; get initial romaji
+      (let ((cnt 0))   ; delete duplicate elements
+        (while (< cnt (length new-candidates))
+          (let* ((elm (nth cnt new-candidates)))
+            (setq new-candidates (delete elm new-candidates))
+            (if (= cnt 0)
+                (setq new-candidates (cons elm new-candidates))
+              (setcdr (nthcdr (1- cnt) new-candidates)
+                      (cons elm (nthcdr cnt new-candidates)))))
+          (setq cnt (1+ cnt))))
+      (setq typeJR-cur-romaji-candidates
+            (append typeJR-cur-romaji-candidates new-candidates))
+      )))
+
+(defun typeJR-deal-with-hatuon ()
+  "Remove \"n\" from candidates of hatuon when next hiragana is na-gyou or boin."
+  (let ((next-hiragana (typeJR-get-next-hiragana)))
+    (when (or (member next-hiragana typeJR-na-gyou)
+              (member next-hiragana typeJR-boin))
+      (setq typeJR-cur-romaji-candidates
+            (delete "n" typeJR-cur-romaji-candidates)))))
+
+(defun typeJR-deal-with-nn-flag ()
+  "\"nn\""
+  (let ((next-hiragana (typeJR-get-next-hiragana)))
+    (when (and (not (member next-hiragana typeJR-na-gyou))
+               (not (member next-hiragana typeJR-boin))
+               (member "n" typeJR-cur-romaji-candidates))
+      (setq typeJR-nn-flag t))))
+
+(defun typeJR-end-of-mondai-p ()
+  ""
+  (<= (length typeJR-cur-mondai) typeJR-cur-hiragana-pos))
+
+(defun typeJR-end-of-hiragana-p ()
+  ""
+  (<= (car (sort (mapcar 'length typeJR-cur-romaji-candidates) '<))
+      typeJR-cur-romaji-pos))
+
+(defun typeJR-get-cur-hiragana ()
+  ""
+  (let* ((len (length typeJR-cur-mondai))
          (two-hiragana
-          (substring typejr-cur-mondai
-                     typejr-cur-hiragana-pos (min (+ 2 typejr-cur-hiragana-pos) len)))
+          (substring typeJR-cur-mondai
+                     typeJR-cur-hiragana-pos (min (+ 2 typeJR-cur-hiragana-pos) len)))
          (one-hiragana
-          (substring typejr-cur-mondai
-                     typejr-cur-hiragana-pos (min (1+ typejr-cur-hiragana-pos) len))))
-    (if (assoc two-hiragana typejr-romaji-definition)
-        (car (assoc two-hiragana typejr-romaji-definition))
-      (car (assoc one-hiragana typejr-romaji-definition)))
+          (substring typeJR-cur-mondai
+                     typeJR-cur-hiragana-pos (min (1+ typeJR-cur-hiragana-pos) len))))
+    (if (assoc two-hiragana typeJR-romaji-definition)
+        (car (assoc two-hiragana typeJR-romaji-definition))
+      (car (assoc one-hiragana typeJR-romaji-definition)))
     ))
 
-(defun typejr-get-correct-candidate-list (romaji)
-  "Pick up correct candidate list from `typejr-cur-romaji-candidates'."
+(defun typeJR-get-next-hiragana ()
+  ""
+  (let ((typeJR-cur-hiragana-pos
+         (+ typeJR-cur-hiragana-pos (length typeJR-cur-hiragana))))
+    (typeJR-get-cur-hiragana)))
+
+(defun typeJR-get-correct-candidate-list (romaji)
+  "Pick up correct candidate list from `typeJR-cur-romaji-candidates'."
   (let (lst)
     (mapc '(lambda (candidate)
-             (when (= (aref candidate typejr-cur-romaji-pos) romaji)
+             (when (= (aref candidate typeJR-cur-romaji-pos) romaji)
                (add-to-list 'lst candidate)))
-          typejr-cur-romaji-candidates)
-    (reverse lst)))
+          typeJR-cur-romaji-candidates)
+    (nreverse lst)))
 
-(defun typejr-get-romaji-candidates (hiragana)
+(defun typeJR-get-romaji-candidates (hiragana)
   "Return list of candidates of romaji definition corresponding to HIRAGANA."
-  (cdr (assoc hiragana typejr-romaji-definition)))
+  (cdr (assoc hiragana typeJR-romaji-definition)))
 
-(defun typejr-read-romaji-definition ()
-  "Read romaji henkan definition from \"romaji-definition.txt\"."
+(defun typeJR-read-mondai-alist ()
+  "Read mondai list from \"mondai-list.txt\"."
+  (setq typeJR-mondai-alist nil)
   (with-temp-buffer
     (let ((coding-system-for-read 'utf-8))
-      (insert-file-contents "romaji-definition.txt")
-      (let ((str (buffer-string)))      ; process buffer per line
-        (mapc '(lambda (line)
-                 (unless (string= line "") ; ignore empty line
-                   (add-to-list 'typejr-romaji-definition (split-string line))))
-              (split-string str "\n"))
-        typejr-romaji-definition))))
+      ;; Is this a good method to find exting file?
+      (insert-file-contents (locate-library "mondai-list.txt"))
+      (let ((str-per-line (split-string (buffer-string) "\n")))  ; process buffer per line
+        (delete "" str-per-line) ; ignore empty line
+        (let ((cnt 0))
+          (while (< cnt (length str-per-line))
+            (add-to-list 'typeJR-mondai-alist (cons (nth cnt str-per-line)
+                                                    (nth (1+ cnt) str-per-line)))
+            (setq cnt (+ 2 cnt))))
+        typeJR-mondai-alist))))
 
-(defun typejr-rewrite-mondai ()
+(defun typeJR-read-romaji-definition ()
+  "Read romaji henkan definition from \"romaji-definition.txt\"."
+  (setq typeJR-romaji-definition nil)
+  (with-temp-buffer
+    (let ((coding-system-for-read 'utf-8))
+      ;; Is this a good method to find exting file?
+      (insert-file-contents (locate-library "romaji-definition.txt"))
+      (let ((str-per-line (split-string (buffer-string) "\n")))  ; process buffer per line
+        (delete "" str-per-line) ; ignore empty line
+        (mapc '(lambda (line)
+                   (add-to-list 'typeJR-romaji-definition (split-string line)))
+              str-per-line)
+        typeJR-romaji-definition))))
+
+(defun typeJR-rewrite-kanji ()
+  ""
+  (save-excursion
+    (goto-char (point-min))
+    (re-search-forward "^$")
+    (unless (= (point) (point-min))
+      (backward-char 1)
+      (delete-region (point-min) (point)))
+    (insert (mapconcat 'eval typeJR-kanji-list "　"))
+    (fill-paragraph)))
+
+(defun typeJR-rewrite-mondai ()
   (save-excursion
     (goto-char (point-min))
     (re-search-forward "ひらがな問題文                ：")
     (delete-region (point) (point-at-eol))
-    (insert typejr-cur-mondai)))
+    (let* ((beg-pt (point))
+           (cur-pt (+ beg-pt typeJR-cur-hiragana-pos)))
+      (insert typeJR-cur-mondai)
+      (put-text-property beg-pt cur-pt 'face 'typeJR-good-romaji-face)
+      (put-text-property cur-pt (min (+ (length typeJR-cur-hiragana) cur-pt)
+                                     (point-at-eol))
+                         'face (if typeJR-wrong-flag
+                                   'typeJR-wrong-romaji-face
+                                 'typeJR-cur-romaji-face)))))
 
-(defun typejr-rewrite-inputed-mondai ()
+(defun typeJR-rewrite-inputed-mondai ()
   (save-excursion
     (goto-char (point-min))
     (re-search-forward "いままで入力した問題文        ：")
     (delete-region (point) (point-at-eol))
-    (insert typejr-cur-inputed-mondai)))
+    (insert typeJR-cur-inputed-mondai)))
 
-(defun typejr-rewrite-hiragana ()
+(defun typeJR-rewrite-hiragana ()
   (save-excursion
     (goto-char (point-min))
     (re-search-forward "次にうつべきひらがな          ：")
     (delete-region (point) (point-at-eol))
-    (insert typejr-cur-hiragana)))
+    (insert typeJR-cur-hiragana)))
 
-(defun typejr-rewrite-romaji-candidates ()
+(defun typeJR-rewrite-romaji-candidates ()
   (save-excursion
     (goto-char (point-min))
     (re-search-forward "次にうつべきローマ字の候補    ：")
     (delete-region (point) (point-at-eol))
     (mapc '(lambda (candidate)
-             (let ((pt (point)))
+             (let* ((beg-pt (point))
+                    (cur-pt (+ beg-pt typeJR-cur-romaji-pos)))
                (insert (concat candidate " "))
-               (put-text-property pt (+ pt typejr-cur-romaji-pos)
-                                  'face 'typejr-good-romaji-face)))
-          typejr-cur-romaji-candidates)))
+               (put-text-property beg-pt cur-pt
+                                  'face 'typeJR-good-romaji-face)
+               (put-text-property cur-pt (1+ cur-pt)
+                                  'face (if typeJR-wrong-flag
+                                            'typeJR-wrong-romaji-face
+                                          'typeJR-cur-romaji-face))))
+          typeJR-cur-romaji-candidates)))
 
-(defun typejr-rewrite-romaji-pos ()
+(defun typeJR-rewrite-romaji-pos ()
   (save-excursion
     (goto-char (point-min))
     (re-search-forward "ローマ字何文字目まで入力したか：")
     (delete-region (point) (point-at-eol))
-    (insert (number-to-string typejr-cur-romaji-pos))))
+    (insert (number-to-string typeJR-cur-romaji-pos))))
 
-(defun typejr-rewrite-evaluation (evaluation)
+(defun typeJR-rewrite-evaluation (evaluation)
   (save-excursion
     (goto-char (point-min))
     (re-search-forward "Evaluation: ")
     (delete-region (point) (point-at-eol))
     (insert evaluation)))
 
+(defun typeJR-clear-type ()
+  (goto-char (point-min))
+  (re-search-forward "Type: ")
+  (delete-region (point) (point-at-eol)))
 
 
+(defun typeJR-stop-watch-start ()
+  (setq typeJR-stop-watch-channel (cc:signal-channel))
+  (cc:signal-connect typeJR-stop-watch-channel 'stop 'typeJR-stop-watch-stop)
+  (unless typeJR-stop-watch-thread
+    (setq typeJR-stop-watch-thread t))
+  (save-excursion
+    (goto-char (point-min))
+    (re-search-forward "^Time: ")
+    (lexical-let ((beg-time (current-time))
+                  (pos (point)))
+      (cc:thread
+       50
+       (message "Stop watch start.")
+       (while typeJR-stop-watch-thread
+         (save-excursion
+           (goto-char pos) (delete-region pos (point-at-eol))
+           (insert (concat (sub-time-string beg-time) " sec"))))))))
 
+(defvar typeJR-stop-watch-thread nil)
+(defvar typeJR-stop-watch-channel nil)
 
+(defun typeJR-stop-watch-stop ()
+  (setq typeJR-stop-watch-thread nil)
+  (message "Stop watch stop."))
+
+(defun sub-time-string (beg-time)
+  (let ((time (current-time)))
+  (format "%6.3f" (+ (- (nth 1 time) (nth 1 beg-time)) ; sec
+                    (/ (- (nth 2 time) (nth 2 beg-time)) 1000000.0))))) ; msec
